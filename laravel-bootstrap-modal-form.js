9efc13ae6c704b51ce8acf8ce4ec4337c910fa7f
Jesse Leite <jesseleite@gmail.com>
2015-06-13 15:11:11 -0400

    Tabs to spaces.

--- laravel-bootstrap-modal-form.js
+++ laravel-bootstrap-modal-form.js
@@ -9,103 +9,103 @@
 
 $('document').ready(function() {
 
-	// Prepare reset.
-	function resetModalFormErrors() {
-		$('.form-group').removeClass('has-error');
-		$('.form-group').find('.help-block').remove();
-	}
+    // Prepare reset.
+    function resetModalFormErrors() {
+        $('.form-group').removeClass('has-error');
+        $('.form-group').find('.help-block').remove();
+    }
 
-	// Intercept submit.
-	$('form.bootstrap-modal-form').on('submit', function(submission) {
-		submission.preventDefault();
+    // Intercept submit.
+    $('form.bootstrap-modal-form').on('submit', function(submission) {
+        submission.preventDefault();
 
-		// Set vars.
-		var form   = $(this),
-		    url    = form.attr('action'),
-		    submit = form.find('[type=submit]');
+        // Set vars.
+        var form   = $(this),
+            url    = form.attr('action'),
+            submit = form.find('[type=submit]');
 
-		// Check for file inputs.
-		if (form.find('[type=file]').length) {
+        // Check for file inputs.
+        if (form.find('[type=file]').length) {
 
-			// If found, prepare submission via FormData object.
-			var input       = form.serializeArray(),
-			    data        = new FormData(),
-			    contentType = false;
+            // If found, prepare submission via FormData object.
+            var input       = form.serializeArray(),
+                data        = new FormData(),
+                contentType = false;
 
-			// Append input to FormData object.
-			$.each(input, function(index, input) {
-				data.append(input.name, input.value);
-			});
+            // Append input to FormData object.
+            $.each(input, function(index, input) {
+                data.append(input.name, input.value);
+            });
 
-			// Append files to FormData object.
-			$.each(form.find('[type=file]'), function(index, input) {
-				if (input.files.length == 1) {
-					data.append(input.name, input.files[0]);
-				} else if (input.files.length > 1) {
-					data.append(input.name, input.files);
-				}
-			});
-		}
+            // Append files to FormData object.
+            $.each(form.find('[type=file]'), function(index, input) {
+                if (input.files.length == 1) {
+                    data.append(input.name, input.files[0]);
+                } else if (input.files.length > 1) {
+                    data.append(input.name, input.files);
+                }
+            });
+        }
 
-		// If no file input found, do not use FormData object (better browser compatibility).
-		else {
-			var data        = form.serialize(),
-			    contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
-		}
+        // If no file input found, do not use FormData object (better browser compatibility).
+        else {
+            var data        = form.serialize(),
+                contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
+        }
 
-		// Please wait.
-		if (submit.is('button')) {
-			var submitOriginal = submit.html();
-			submit.html('Please wait...');
-		} else if (submit.is('input')) {
-			var submitOriginal = submit.val();
-			submit.val('Please wait...');
-		}
+        // Please wait.
+        if (submit.is('button')) {
+            var submitOriginal = submit.html();
+            submit.html('Please wait...');
+        } else if (submit.is('input')) {
+            var submitOriginal = submit.val();
+            submit.val('Please wait...');
+        }
 
-		// Request.
-		$.ajax({
-			type: "POST",
-			url: url,
-			data: data,
-			dataType: 'json',
-			cache: false,
-			contentType: contentType,
-			processData: false
+        // Request.
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: data,
+            dataType: 'json',
+            cache: false,
+            contentType: contentType,
+            processData: false
 
-		// Response.
-		}).always(function(response, status) {
+        // Response.
+        }).always(function(response, status) {
 
-			// Reset errors.
-			resetModalFormErrors();
+            // Reset errors.
+            resetModalFormErrors();
 
-			// Check for errors.
-			if (response.status == 422) {
-				var errors = $.parseJSON(response.responseText);
+            // Check for errors.
+            if (response.status == 422) {
+                var errors = $.parseJSON(response.responseText);
 
-				// Iterate through errors object.
-				$.each(errors, function(field, message) {
-					console.error(field+': '+message);
-					var formGroup = $('[name='+field+']', form).closest('.form-group');
-					formGroup.addClass('has-error').append('<p class="help-block">'+message+'</p>');
-				});
+                // Iterate through errors object.
+                $.each(errors, function(field, message) {
+                    console.error(field+': '+message);
+                    var formGroup = $('[name='+field+']', form).closest('.form-group');
+                    formGroup.addClass('has-error').append('<p class="help-block">'+message+'</p>');
+                });
 
-				// Reset submit.
-				if (submit.is('button')) {
-					submit.html(submitOriginal);
-				} else if (submit.is('input')) {
-					submit.val(submitOriginal);
-				}
+                // Reset submit.
+                if (submit.is('button')) {
+                    submit.html(submitOriginal);
+                } else if (submit.is('input')) {
+                    submit.val(submitOriginal);
+                }
 
-			// If successful, reload.
-			} else {
-				location.reload();
-			}
-		});
-	});
+            // If successful, reload.
+            } else {
+                location.reload();
+            }
+        });
+    });
 
-	// Reset errors when opening modal.
-	$('.bootstrap-modal-form-open').click(function() {
-		resetModalFormErrors();
-	});
+    // Reset errors when opening modal.
+    $('.bootstrap-modal-form-open').click(function() {
+        resetModalFormErrors();
+    });
 
 });
